b1a70b8b23924635dbb6369fdab5af187f3b6320
davidcawork <davidcawork@gmail.com>
2021-06-24 12:48:55 +0200

    Added func to determinate losses

--- src/graph/link.py
+++ src/graph/link.py
@@ -9,6 +9,10 @@ class Link(object):
     NORMAL = 1
     SWITCH = 0
 
+    # Vamos a definir constantes que son propias del enlace
+    VOLTAGE = 400  # Volts
+    SWITCH_R = 0.1  # Ohms
+
     def __init__(self, type_link, state, dist, cap):
         """
             Constructor de la clase Link
@@ -17,6 +21,7 @@ class Link(object):
         self.state = state
         self.dist = dist
         self.cap = cap
+        self.coef_R = 1  # Ohms/km (De momento, no lo tenemos)
 
     @staticmethod
     def ft2meters(fts):
@@ -31,3 +36,19 @@ class Link(object):
             Funcion de conversion a metros
         """
         return meters * 3.28084
+
+    @staticmethod
+    def getLosses_Switch(P_in):
+        """
+            Función para calcular las perdidas de inserción por un switch activo dada una Potencia incidente (kW - Kilowatts)
+        """
+        return (((Link.SWITCH_R)/(Link.VOLTAGE)**2) * (P_in)**2)
+
+    def getLosses_Link(self, P_in):
+        """
+            Función para calcular las perdidas de un enlace dada una potencia incidente (kW - kilowatts)
+        """
+
+        r_eff = self.coef_R * (Link.ft2meters(self.dist)/1000)  # El coef_R esta en ohms/km -> la distancia nos venía en fts
+
+        return (((r_eff)/(Link.VOLTAGE)**2) * (P_in)**2)
