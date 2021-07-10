aa693aa6a4c74c3ab13f0e5a3da565d44e44277b
davidcawork <davidcawork@gmail.com>
2021-07-10 23:58:54 +0200

    added auto test

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
+import pathlib
 from graph.graph import Graph
 from den2ne.den2neALG import Den2ne
 from dataCollector.dataCollector import DataGatherer
 
 
-def main():
+# Este es un ejemplo de uso de todo el repertorio de metodos programados para explorar la topología IEEE 123 y
+# el algoritmo programado.
+
+
+def usage():
 
     # Recolectamos los datos
     loads = DataGatherer.getLoads('data/loads.csv', 3)
@@ -61,5 +66,85 @@ def main():
     G.showGraph()
 
 
+# Vamos a programar unas pruebas globales sobre la topología IEEE 123
+def test_ieee123():
+
+    # Variables
+    dirs = ['reports', 'csv', 'fig']
+    topo_name = 'ieee123'
+    criteria = [Den2ne.CRITERION_NUM_HOPS, Den2ne.CRITERION_DISTANCE, Den2ne.CRITERION_LINKS_LOSSES,
+                Den2ne.CRITERION_POWER_BALANCE, Den2ne.CRITERION_POWER_BALANCE_WITH_LOSSES]
+    out_data = dict()
+
+    # Preparamos los directorios de resultados
+    for dir in dirs:
+        pathlib.Path('results/' + topo_name + '/' + dir).mkdir(parents=True, exist_ok=True)
+
+    # Recolectamos los datos
+    loads = DataGatherer.getLoads('data/loads.csv', 3)
+    edges = DataGatherer.getEdges('data/links.csv')
+    edges_conf = DataGatherer.getEdges_Config('data/links_config.csv')
+    sw_edges = DataGatherer.getSwitches('data/switches.csv')
+    positions = DataGatherer.getPositions('data/node_positions.csv')
+
+    # Creamos la var del grafo para el primer instante
+    G = Graph(0, loads, edges, sw_edges, edges_conf, root='150')
+
+    # Podamos los nodos virtuales que estén a modo de ampliación.
+    G.pruneGraph()
+
+    # Iniciamos el algoritmo
+    G_den2ne_alg = Den2ne(G)
+
+    # Primera fase: difusión de IDs
+    G_den2ne_alg.spread_ids()
+
+    # Vamos a iterar por todos los intantes de cargas
+    for delta in range(0, len(loads['1'])):
+
+        # Vamos a iterar por criterio
+        for criterion in criteria:
+
+            # Init Loads
+            G_den2ne_alg.updateLoads(loads, delta)
+            G_den2ne_alg.clearSelectedIDs()
+            G_den2ne_alg.selectBestIDs(criterion)
+
+            # Ideal balance
+            [total_balance_ideal, abs_flux] = G_den2ne_alg.globalBalance(withLosses=False, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+
+            # Re-Init loads
+            G_den2ne_alg.updateLoads(loads, delta)
+            G_den2ne_alg.clearSelectedIDs()
+            G_den2ne_alg.selectBestIDs(criterion)
+
+            # Withloss balance
+            [total_balance_with_losses, abs_flux_with_losses] = G_den2ne_alg.globalBalance(withLosses=True, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+
+            # Re-Init loads
+            G_den2ne_alg.updateLoads(loads, delta)
+            G_den2ne_alg.clearSelectedIDs()
+            G_den2ne_alg.selectBestIDs(criterion)
+
+            # Withloss and Cap balance
+            [total_balance_with_lossesCap, abs_flux_with_lossesCap] = G_den2ne_alg.globalBalance(withLosses=True, withCap=True, withDebugPlot=False, positions=positions, path='results/')
+
+            # Save data
+            out_data[delta][criteria] = {
+                'total_balance_ideal': total_balance_ideal, 'abs_flux': abs_flux,
+                'total_balance_with_losses': total_balance_with_losses, 'abs_flux_with_losses': abs_flux_with_losses,
+                'total_balance_with_lossesCap': total_balance_with_lossesCap, 'abs_flux_with_lossesCap': abs_flux_with_lossesCap
+            }
+
+            # Genearación de informes
+            G_den2ne_alg.write_swConfig_report(f'results/{topo_name}/report_swConfig_{delta}_{criterion}.txt')
+            G_den2ne_alg.write_loads_report(f'results/{topo_name}/reports/report_loads_{delta}_{criterion}.txt')
+
+            # Generamos la configuración logica
+            G_den2ne_alg.write_swConfig_CSV(f'results/{topo_name}/csv/swConfig_{delta}_{criterion}.csv')
+
+    G_den2ne_alg.write_ids_report(f'results/{topo_name}/reports/report_ids.txt')
+
+
 if __name__ == "__main__":
-    main()
+    test_ieee123()
