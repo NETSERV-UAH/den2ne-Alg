e66123a499b5bb1abb3f4fdd078eb36efea08ce4
davidcawork <davidcawork@gmail.com>
2021-12-14 11:59:49 +0100

    Added old results

---
+++ src/results/old/plotter.m
@@ -0,0 +1,18 @@
+%% Plotter.m
+%
+%   [+] Autor: David Carrascal <david.carrascal@uah.es> 
+%
+%   [+] Fecha: 11 Jul 2021
+clc
+close all
+clear varibles
+
+%% Main
+
+% Pintamos las graficas correspondientes al balance global de potencias, el
+% valor absoluto del flujo de potencia, perdias por enlaces - switches,
+% perdidas por superar el valor máximo de la configuración de un enlace 
+plotDeltaLoads(0)
+
+% Pintamos el valor medio de todos los intantes de carga 
+plotRangeLoads(0, 95)
