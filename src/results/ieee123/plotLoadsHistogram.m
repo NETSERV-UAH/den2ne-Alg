9590b41c436fe255da65a76abf78c5bf80ec5ec4
davidcawork <davidcawork@gmail.com>
2024-05-20 11:27:01 +0200

    Added load distribution histogram

---
+++ src/results/ieee123/plotLoadsHistogram.m
@@ -0,0 +1,37 @@
+%% Plotter.m
+%
+%   [+] Autor: David Carrascal <david.carrascal@uah.es> 
+%
+%   [+] Fecha: 20 May 2024  
+clc
+close all
+clear varibles
+
+
+%% Main
+
+% First we have to indicate where are the loads distribution
+PATH_LOADS_DIST = "../../data/";
+
+% Lets read the csv with all the info
+data_table = readtable(strcat(PATH_LOADS_DIST,"loads.csv"), 'NumHeaderLines',1);
+    
+% Parse from table to matrix 
+data = data_table{:,2:end};
+
+% Flatten the matrix to a single vector
+data_vector = data(:);
+
+% Plot histogram
+h=figure();
+set(gcf,'Position',[100 100 900 700]);
+histogram(data_vector);
+grid on
+title('Distribution of end node load profiles','FontSize',16);
+xlabel('Power (kW)');
+ylabel('Frequency');
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+strPath = strcat('fig/', 'loadsHist');
+print(h,strPath,'-dpdf','-r0')
