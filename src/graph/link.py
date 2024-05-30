97e202a29f2642261914bd1be6998c9f0f81b9b9
davidcawork <davidcawork@gmail.com>
2024-05-30 11:05:43 +0200

    lets prepare the test

--- src/graph/link.py
+++ src/graph/link.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 class Link(object):
     """
-        Clase para gestionar un enlace del grafo
+    Clase para gestionar un enlace del grafo
     """
 
     # Declaramos tipos de enlace mediante variables estáticas de la clase
@@ -15,7 +16,7 @@ class Link(object):
 
     def __init__(self, node_a, node_b, type_link, state, dist, conf, coef_r, i_max):
         """
-            Constructor de la clase Link
+        Constructor de la clase Link
         """
         self.node_a = node_a
         self.node_b = node_b
@@ -24,35 +25,34 @@ class Link(object):
         self.state = state
         self.dist = dist
         self.conf = conf
-        
-        # Según nos han indicado los enlaces de tipo switch no tienen dist, cap 
+
+        # Según nos han indicado los enlaces de tipo switch no tienen dist, cap
         if self.type != Link.SWITCH:
-            self.capacity = (i_max * Link.VOLTAGE)/1000   # kW
-            self.coef_R = coef_r                          # Ohms/km 
+            self.capacity = (i_max * Link.VOLTAGE) / 1000  # kW
+            self.coef_R = coef_r  # Ohms/km
         else:
             self.capacity = None
             self.coef_R = None
 
-
     @staticmethod
     def ft2meters(fts):
         """
-            Funcion de conversion a feet (american unit)
+        Funcion de conversion a feet (american unit)
         """
-        return (fts)/3.28084
+        return (fts) / 3.28084
 
     @staticmethod
     def meters2ft(meters):
         """
-            Funcion de conversion a metros
+        Funcion de conversion a metros
         """
         return meters * 3.28084
 
     def getLosses(self, Pin):
         """
-            Función para calcular las perdidas de un enlace de forma agnostica
+        Función para calcular las perdidas de un enlace de forma agnostica
         """
-        Loss = 0.0   # kW
+        Loss = 0.0  # kW
 
         if self.type == Link.SWITCH:
             Loss = Link.getLosses_Switch(Pin)
@@ -64,15 +64,17 @@ class Link(object):
     @staticmethod
     def getLosses_Switch(P_in):
         """
-            Función para calcular las perdidas de inserción por un switch activo dada una Potencia incidente (kW - Kilowatts)
+        Función para calcular las perdidas de inserción por un switch activo dada una Potencia incidente (kW - Kilowatts)
         """
-        return ((((Link.SWITCH_R)/(Link.VOLTAGE)**2) * (abs(P_in)*1000)**2)/1000)
+        return (((Link.SWITCH_R) / (Link.VOLTAGE) ** 2) * (P_in * 1000) ** 2) / 1000
 
     def getLosses_Link(self, P_in):
         """
-            Función para calcular las perdidas de un enlace dada una potencia incidente (kW - kilowatts)
+        Función para calcular las perdidas de un enlace dada una potencia incidente (kW - kilowatts)
         """
 
-        r_eff = self.coef_R * (Link.ft2meters(self.dist)/1000)  # El coef_R esta en ohms/km -> la distancia nos venía en fts
+        r_eff = self.coef_R * (
+            Link.ft2meters(self.dist) / 1000
+        )  # El coef_R esta en ohms/km -> la distancia nos venía en fts
 
-        return ((((r_eff)/(Link.VOLTAGE)**2) * (abs(P_in)*1000)**2)/1000)
+        return (((r_eff) / (Link.VOLTAGE) ** 2) * (P_in * 1000) ** 2) / 1000
--- src/test_ieee123.py
+++ src/test_ieee123.py
@@ -13,23 +13,23 @@ from dataCollector.dataCollector import DataGatherer
 def usage():
 
     # Recolectamos los datos
-    loads = DataGatherer.getLoads('data/load_v2.csv', 3)
-    edges = DataGatherer.getEdges('data/links.csv')
-    edges_conf = DataGatherer.getEdges_Config('data/links_config.csv')
-    sw_edges = DataGatherer.getSwitches('data/switches.csv')
-    positions = DataGatherer.getPositions('data/node_positions.csv')
+    loads = DataGatherer.getLoads("data/load_v2.csv", 3)
+    edges = DataGatherer.getEdges("data/links.csv")
+    edges_conf = DataGatherer.getEdges_Config("data/links_config.csv")
+    sw_edges = DataGatherer.getSwitches("data/switches.csv")
+    positions = DataGatherer.getPositions("data/node_positions.csv")
 
     # Creamos la var del grafo para el primer instante
-    G = Graph(0, loads, edges, sw_edges, edges_conf, 'prueba.json', root='150')
+    G = Graph(0, loads, edges, sw_edges, edges_conf, "prueba.json", root="150")
 
     # Parseamos a NetworkX y pintamos el grafo
-    G.plotGraph(positions, 'IEEE 123 Node test feeder - Graph')
+    G.plotGraph(positions, "IEEE 123 Node test feeder - Graph")
 
     # Podamos los nodos virtuales que estén a modo de ampliación.
     G.pruneGraph()
 
     # Podemos vovler a pintar para comprobar la poda realziada
-    G.plotGraph(positions, ' IEEE 123 Node test feeder - Pruned Graph')
+    G.plotGraph(positions, " IEEE 123 Node test feeder - Pruned Graph")
 
     # Iniciamos el algoritmo
     G_den2ne_alg = Den2ne(G)
@@ -41,57 +41,77 @@ def usage():
     G_den2ne_alg.selectBestIDs(Den2ne.CRITERION_POWER_BALANCE_WEIGHTED)
 
     # Tercera fase: Balance global de la red y establece los flujos de potencia
-    [total_balance_ideal, abs_flux] = G_den2ne_alg.globalBalance(withLosses=False, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+    [total_balance_ideal, abs_flux] = G_den2ne_alg.globalBalance(
+        withLosses=False,
+        withCap=False,
+        withDebugPlot=False,
+        positions=positions,
+        path="results/",
+    )
 
     # Podemos calcular las perdidas totales:
     G_den2ne_alg.updateLoads(loads, 0)
     G_den2ne_alg.clearSelectedIDs()
     G_den2ne_alg.selectBestIDs(Den2ne.CRITERION_NUM_HOPS)
-    [total_balance_with_losses, abs_flux_with_losses] = G_den2ne_alg.globalBalance(withLosses=True, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+    [total_balance_with_losses, abs_flux_with_losses] = G_den2ne_alg.globalBalance(
+        withLosses=True,
+        withCap=False,
+        withDebugPlot=False,
+        positions=positions,
+        path="results/",
+    )
 
     loss = total_balance_ideal - total_balance_with_losses
 
     # Pintamos de nuevo para ver los flujos de potencia
-    G_den2ne_alg.G.plotDiGraph(positions, 'IEEE 123 Node test feeder - Directed Graph')
+    G_den2ne_alg.G.plotDiGraph(positions, "IEEE 123 Node test feeder - Directed Graph")
 
     # Genearación de informes
-    G_den2ne_alg.write_ids_report('results/reports/report_ids.txt')
-    G_den2ne_alg.write_swConfig_report('results/reports/report_swConfig.txt')
-    G_den2ne_alg.write_loads_report('results/reports/report_loads.txt')
+    G_den2ne_alg.write_ids_report("results/reports/report_ids.txt")
+    G_den2ne_alg.write_swConfig_report("results/reports/report_swConfig.txt")
+    G_den2ne_alg.write_loads_report("results/reports/report_loads.txt")
 
     # Generamos la configuración logica en formato CSV para su posterior procesado en Matlab
-    G_den2ne_alg.write_swConfig_CSV('results/swConfig.csv')
+    G_den2ne_alg.write_swConfig_CSV("results/swConfig.csv")
 
     # Sacamos las figuras en modo iteractivo (Metodo que bloquea el flujo del script)
     G.showGraph()
 
-    #Voy a probar aqui hacer el json
-    G.saveGraph('prueba1.json')
+    # Voy a probar aqui hacer el json
+    G.saveGraph("prueba1.json")
+
 
 # Vamos a programar unas pruebas globales sobre la topología IEEE 123
 def test_ieee123():
 
     # Variables
-    dirs = ['reports', 'csv', 'fig']
-    topo_name = 'ieee123'
-    criteria = [Den2ne.CRITERION_NUM_HOPS, Den2ne.CRITERION_DISTANCE, Den2ne.CRITERION_LINKS_LOSSES,
-                Den2ne.CRITERION_POWER_BALANCE, Den2ne.CRITERION_POWER_BALANCE_WITH_LOSSES,
-                Den2ne.CRITERION_POWER_BALANCE_WEIGHTED]
+    dirs = ["reports", "csv", "fig"]
+    topo_name = "ieee123"
+    criteria = [
+        Den2ne.CRITERION_NUM_HOPS,
+        Den2ne.CRITERION_DISTANCE,
+        Den2ne.CRITERION_LINKS_LOSSES,
+        Den2ne.CRITERION_POWER_BALANCE,
+        Den2ne.CRITERION_POWER_BALANCE_WITH_LOSSES,
+        Den2ne.CRITERION_POWER_BALANCE_WEIGHTED,
+    ]
     out_data = dict()
 
     # Preparamos los directorios de resultados
     for dir in dirs:
-        pathlib.Path('results/' + topo_name + '/' + dir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path("results/" + topo_name + "/" + dir).mkdir(
+            parents=True, exist_ok=True
+        )
 
     # Recolectamos los datos
-    loads = DataGatherer.getLoads('data/load_v2.csv', 3)
-    edges = DataGatherer.getEdges('data/links.csv')
-    edges_conf = DataGatherer.getEdges_Config('data/links_config.csv')
-    sw_edges = DataGatherer.getSwitches('data/switches.csv')
-    positions = DataGatherer.getPositions('data/node_positions.csv')
+    loads = DataGatherer.getLoads("data/loads_v2.csv", 3)
+    edges = DataGatherer.getEdges("data/links.csv")
+    edges_conf = DataGatherer.getEdges_Config("data/links_config.csv")
+    sw_edges = DataGatherer.getSwitches("data/switches.csv")
+    positions = DataGatherer.getPositions("data/node_positions.csv")
 
     # Creamos la var del grafo para el primer instante
-    G = Graph(0, loads, edges, sw_edges, edges_conf, root='150')
+    G = Graph(0, loads, edges, sw_edges, edges_conf, root="150")
 
     # Podamos los nodos virtuales que estén a modo de ampliación.
     G.pruneGraph()
@@ -103,7 +123,7 @@ def test_ieee123():
     G_den2ne_alg.spread_ids()
 
     # Vamos a iterar por todos los intantes de cargas
-    for delta in range(0, len(loads['1'])):
+    for delta in range(0, len(loads["1"])):
 
         out_data[delta] = dict()
 
@@ -116,7 +136,13 @@ def test_ieee123():
             G_den2ne_alg.selectBestIDs(criterion)
 
             # Ideal balance
-            [total_balance_ideal, abs_flux] = G_den2ne_alg.globalBalance(withLosses=False, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+            [total_balance_ideal, abs_flux] = G_den2ne_alg.globalBalance(
+                withLosses=False,
+                withCap=False,
+                withDebugPlot=False,
+                positions=positions,
+                path="results/",
+            )
 
             # Re-Init loads
             G_den2ne_alg.updateLoads(loads, delta)
@@ -124,7 +150,15 @@ def test_ieee123():
             G_den2ne_alg.selectBestIDs(criterion)
 
             # Withloss balance
-            [total_balance_with_losses, abs_flux_with_losses] = G_den2ne_alg.globalBalance(withLosses=True, withCap=False, withDebugPlot=False, positions=positions, path='results/')
+            [total_balance_with_losses, abs_flux_with_losses] = (
+                G_den2ne_alg.globalBalance(
+                    withLosses=True,
+                    withCap=False,
+                    withDebugPlot=False,
+                    positions=positions,
+                    path="results/",
+                )
+            )
 
             # Re-Init loads
             G_den2ne_alg.updateLoads(loads, delta)
@@ -132,33 +166,57 @@ def test_ieee123():
             G_den2ne_alg.selectBestIDs(criterion)
 
             # Withloss and Cap balance
-            [total_balance_with_lossesCap, abs_flux_with_lossesCap] = G_den2ne_alg.globalBalance(withLosses=True, withCap=True, withDebugPlot=False, positions=positions, path='results/')
+            [total_balance_with_lossesCap, abs_flux_with_lossesCap] = (
+                G_den2ne_alg.globalBalance(
+                    withLosses=True,
+                    withCap=True,
+                    withDebugPlot=False,
+                    positions=positions,
+                    path="results/",
+                )
+            )
 
             # Save data
             out_data[delta][criterion] = {
-                'total_balance_ideal': total_balance_ideal, 'abs_flux': abs_flux,
-                'total_balance_with_losses': total_balance_with_losses, 'abs_flux_with_losses': abs_flux_with_losses,
-                'total_balance_with_lossesCap': total_balance_with_lossesCap, 'abs_flux_with_lossesCap': abs_flux_with_lossesCap
+                "total_balance_ideal": total_balance_ideal,
+                "abs_flux": abs_flux,
+                "total_balance_with_losses": total_balance_with_losses,
+                "abs_flux_with_losses": abs_flux_with_losses,
+                "total_balance_with_lossesCap": total_balance_with_lossesCap,
+                "abs_flux_with_lossesCap": abs_flux_with_lossesCap,
             }
 
             # Genearación de informes
-            G_den2ne_alg.write_swConfig_report(f'results/{topo_name}/reports/report_swConfig_d{delta}_c{criterion}.txt')
-            G_den2ne_alg.write_loads_report(f'results/{topo_name}/reports/report_loads_d{delta}_c{criterion}.txt')
+            G_den2ne_alg.write_swConfig_report(
+                f"results/{topo_name}/reports/report_swConfig_d{delta}_c{criterion}.txt"
+            )
+            G_den2ne_alg.write_loads_report(
+                f"results/{topo_name}/reports/report_loads_d{delta}_c{criterion}.txt"
+            )
 
             # Generamos la configuración logica
-            G_den2ne_alg.write_swConfig_CSV(f'results/{topo_name}/csv/swConfig_d{delta}_c{criterion}.csv')
-            
+            G_den2ne_alg.write_swConfig_CSV(
+                f"results/{topo_name}/csv/swConfig_d{delta}_c{criterion}.csv"
+            )
 
         # Exportamos los datos para un valor de delta
-        with open(f'results/{topo_name}/csv/outdata_d{delta}.csv', 'w') as file:
-            file.write('criterion,power_ideal,abs_ideal,power_wloss,abs_wloss,power_wlossCap,abs_wlossCap\n')
+        with open(f"results/{topo_name}/csv/outdata_d{delta}.csv", "w") as file:
+            file.write(
+                "criterion,power_ideal,abs_ideal,power_wloss,abs_wloss,power_wlossCap,abs_wlossCap\n"
+            )
             for criterion in out_data[delta]:
-                file.write(f'{criterion},{out_data[delta][criterion]["total_balance_ideal"]},{out_data[delta][criterion]["abs_flux"]},')
-                file.write(f'{out_data[delta][criterion]["total_balance_with_losses"]}, {out_data[delta][criterion]["abs_flux_with_losses"]},')
-                file.write(f'{out_data[delta][criterion]["total_balance_with_lossesCap"]},{out_data[delta][criterion]["abs_flux_with_lossesCap"]}\n')
+                file.write(
+                    f'{criterion},{out_data[delta][criterion]["total_balance_ideal"]},{out_data[delta][criterion]["abs_flux"]},'
+                )
+                file.write(
+                    f'{out_data[delta][criterion]["total_balance_with_losses"]}, {out_data[delta][criterion]["abs_flux_with_losses"]},'
+                )
+                file.write(
+                    f'{out_data[delta][criterion]["total_balance_with_lossesCap"]},{out_data[delta][criterion]["abs_flux_with_lossesCap"]}\n'
+                )
 
-    G_den2ne_alg.write_ids_report(f'results/{topo_name}/reports/report_ids.txt')
-    
+    G_den2ne_alg.write_ids_report(f"results/{topo_name}/reports/report_ids.txt")
+
 
 if __name__ == "__main__":
     test_ieee123()
